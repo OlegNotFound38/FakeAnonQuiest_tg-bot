858c91e72206095ddb4a0ac9b8bc65af1f11513d
kelfy <worrkingqwe15@gmail.com>
2026-09-13 02:29:57 +0300

    добавил кнопку старт, изменил комментарии

--- main.py
+++ main.py
@@ -8,14 +8,28 @@
 прост коменты, обычным цветом это объясняю че за че отвечает или че делат
 чтобы Вова с Макаром быстрее втянулись
 '''
-
+# telebot и types нужны для работы с Telegram-ботом
+#pathlib нужен для работы с путями к файлам
 from pathlib import Path
-import telebot;             # импорты, чтобы питон понял что мы работаем с
-from telebot import types;  # тг ботами
 
-TOKEN = (Path(__file__).parent / "bot_API.txt").read_text(encoding="utf-8").strip()
+import telebot
+from telebot import types
+
+TOKEN = (Path(__file__).parent / "bot_API.txt").read_text(encoding="utf-8").strip() #токен получаем из файла bot_API.txt
 if not TOKEN:
     raise RuntimeError("API пока не получен")
 
 bot = telebot.TeleBot(TOKEN) # токен/айпи от нашего бота. код отрабатывает именно тот бот, токен которого тут
 bot.polling(none_stop=True, interval=0) # строка чтобы бот не отключался
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(types.KeyboardButton("Начать"))
+
+    bot.send_message(
+        message.chat.id,
+        "Выбери действие:",
+        reply_markup=keyboard,
+    )
